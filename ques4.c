8e48f322c65299bf398312b8cf9fe0c597cc4a71
mansirajpoot <MANSI.41408@STU.UPES.AC.IN>
2026-08-16 12:29:13 +0530

    Add files via upload

---
+++ ques4.c
@@ -0,0 +1,31 @@
+//Q4: Write a program to calculate the area and circumference of a circle given its radius.
+
+/*
+Sample Test Cases:
+Input 1:
+7
+Output 1:
+Area=153.94, Circumference=43.96
+
+Input 2:
+3
+Output 2:
+Area=28.27, Circumference=18.85
+
+*/
+
+#include <stdio.h>
+
+int main () {
+	float radius, area, circumference;
+	const float PI = 3.14159;
+
+	scanf("%f", &radius);
+
+	area = PI * radius * radius;
+	circumference = 2 * PI * radius;
+
+	printf("area=%.2f, circumference=%.2f", area, circumference);
+
+	return 0;
+}
